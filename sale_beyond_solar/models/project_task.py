22c684c5292e18557caf24e9e88464561890b0e2
Jake Robinson <jake.robinson@live.com.au>
2020-07-10 08:29:21 +1000

    FIX: Use correct ralted fields

--- sale_beyond_solar/models/project_task.py
+++ sale_beyond_solar/models/project_task.py
@@ -6,5 +6,5 @@ class ProjectTask(models.Model):
 
     has_battery = fields.Boolean(string="Has Battery", related='sale_order_id.has_battery')
     has_micro_inverter = fields.Boolean(string="Has Micro Inverter", related='sale_order_id.has_micro_inverter')
-    has_inverter = fields.Boolean(string="Has Inverter", related='sale_order_id.has_battery')
-    has_panel = fields.Boolean(string="Has Panel", related='sale_order_id.has_battery')
+    has_inverter = fields.Boolean(string="Has Inverter", related='sale_order_id.has_inverter')
+    has_panel = fields.Boolean(string="Has Panel", related='sale_order_id.has_panel')
