374e0ed7ea66d12b55811606c43908f8c848259a
Jake <git@programmedbyjake.com>
2020-08-04 11:14:28 +1000

    FIX: Prevent TypeError

--- project_beyond_solar/models/sale_order_line.py
+++ project_beyond_solar/models/sale_order_line.py
@@ -7,5 +7,8 @@ class SaleOrderLine(models.Model):
     def _timesheet_create_task_prepare_values(self, project):
         res = super(SaleOrderLine, self)._timesheet_create_task_prepare_values(project)
         if project.sequence_id:
-            res['name'] = project.sequence_id._next() + ' - ' + self.order_id.partner_id.name
+            if self.order_id.partner_id:
+                res['name'] = project.sequence_id._next() + ' - ' + self.order_id.partner_id.name
+            else:
+                res['name'] = project.sequence_id._next()
         return res
